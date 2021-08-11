c0bd711c9295a11671106d8ed7959c29b457bfe1
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-08-11 17:40:36 +0300

    www

--- server.js
+++ server.js
@@ -1,47 +1,24 @@
 
 const express = require("express");
 const app = express();
-const data = require("./data/weather.json");
+require('dotenv').config();
+const cors=require('cors')
+app.use(cors());
+const port=process.env.PORT;
 
-let newArray=[];
+let weatherController=require('./weather.controller')
 
 app.get("/", function (req, res) {
   res.send("hello ");
 });
-app.get("/weather", function (req, res) {
-  let queryCity;
-  req.query["q"]?queryCity=req.query["q"].toLowerCase():queryCity=''
-  let theCity = data.find((city) => {
-    return (
-      Number(req.query["lat"]) === city.lat||
-      Number(req.query["lon"]) === city.lon ||
-      queryCity === city.city_name.toLowerCase() &&
-      city
-    );
-  });
-    if(!theCity){res.status(500).send("The City You Search About It Not Found")}
-  else
- { theCity.data.forEach(elem=>{
-
-    newArray.push(new Forecast(`elem.valid_date,low of ${elem.low_temp} high of ${elem.max_temp}  ${elem.weather.description}`));
-  })
-
-   res.json(newArray); }
-
-
-});
+app.get("/weather",weatherController);
 
 
 
-class Forecast  {
-    constructor(date,description){
-        this.date=date;
-        this.description=description;
-    }
-}
 
 
-app.listen(8000, () => {
+
+app.listen(port, () => {
   console.log("In 8000");
 });
 
